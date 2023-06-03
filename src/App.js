6bc4c89d756f30c0cd9e06603db8bd0c2b71eefb
Moamen Hamed <moamen.hamed33322@gmail.com>
2023-06-03 02:03:22 +0200

    task completed

--- src/App.js
+++ src/App.js
@@ -1,48 +1,99 @@
-import './App.css';
-import React, {useState} from 'react';
-import {marked} from 'marked'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { marked } from "marked";
+import syntax from "./basic-syntax.json";
+
+function useLocalStorage(key, initialValue) {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
+}
 
 const App = () => {
-  const [code, setCode] = useState('## Hello')
-  const [compiled, setCompiled] = useState('<h2 id="hello">Hello</h2>')
-  const [hide, hidePreview] = useState(true)
+  const [code, setCode] = useLocalStorage("init", "## Hello");
+  const [compiled, setCompiled] = useState(marked.parse(code));
+  const [activeTab, setActiveTab] = useState("Editor");
 
   const openMD = () => {
-    console.log(0)
-    hidePreview(true)
-  }
+    console.log(0);
+    setActiveTab("Editor");
+  };
 
   const openPreview = () => {
-    console.log(0)
-    hidePreview(false)
-  }
+    console.log(0);
+    setActiveTab("Preview");
+  };
+  const openDocs = () => {
+    console.log(0);
+    setActiveTab("Docs");
+  };
 
   const handleChange = (e) => {
-    setCode(e.target.value)
-    setCompiled(marked.parse(e.target.value))
-  }
+    setCode(e.target.value);
+    setCompiled(marked.parse(e.target.value));
+  };
+
+  const jsonSyntax = syntax.basic_syntax;
 
   return (
     <>
       <h1>MarkDown Previewer React App</h1>
       <div className="container">
         <div className="btns">
-          <button onClick={openMD} className="btn">MarkDown</button>
+          <button onClick={openMD} className="btn">
+            MarkDown
+          </button>
           <button onClick={openPreview}>Preview</button>
+          <button onClick={openDocs}>DOCS</button>
         </div>
-        {
-        hide ? 
+        {activeTab === "Editor" && (
           <div>
-            <textarea onChange={handleChange} value={code}/>
-          </div> : 
-          <div>
-            <textarea value={compiled}/>
+            <textarea onChange={handleChange} value={code} />
           </div>
-        }
+        )}
+        {activeTab === "Preview" && (
+          <div>
+            <textarea value={compiled} readOnly />
+          </div>
+        )}
+
+        {activeTab === "Docs" && (
+          <div>
+            {jsonSyntax.map((obj, index) => {
+              return (
+                <div className="examples" key={index}>
+                  <h2>{obj.name}</h2>
+                  <p>{obj.description}</p>
+                  <h3>Examples:</h3>
+                  {obj.examples.map((ex, exIndex) => {
+                    return (
+                      <div key={exIndex}>
+                        <code>{ex.markdown}</code>
+
+                        <div>
+                          <code>{ex.html}</code>
+                        </div>
+                        {obj.additional_examples.value && (
+                          <div>
+                            <code>{obj.additional_examples}</code>
+                          </div>
+                        )}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
